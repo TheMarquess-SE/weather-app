792807ad99c68d5b176bbb1b2ee8d91f3b906069
Alejandro Garcia <painisprep@gmail.com>
2025-08-17 23:46:16 -0400

    refactor(weather-api): move weather API inside the new services dir

---
+++ src/services/weather-api.js
@@ -0,0 +1,13 @@
+const API_KEY = 'H2SKW3EZ4PS469UHPXM2JWW7F';
+const BASE_URL =
+  'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+
+export default async function fetchWeather(location) {
+  const url = `${BASE_URL}${location}/?key=${API_KEY}&contentType=json&elements=%2Baqius`;
+  const response = await fetch(url, { mode: 'cors' });
+  if (!response.ok) {
+    throw new Error(`Weather data not found. Status: ${response.status}`);
+  }
+  const weatherRawData = await response.json();
+  return weatherRawData;
+}
