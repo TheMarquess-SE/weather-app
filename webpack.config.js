36dd3164ca8e711812618378c2d250f7732ae62f
Alejandro Garcia <painisprep@gmail.com>
2025-08-22 23:22:12 -0400

    eslint and prettier applied

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-//Webpack config
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// Webpack config
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ['./src/template.html'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: './src/template.html',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
     ],
   },
